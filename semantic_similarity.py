3cda6a60656c1cb7ea134e598fe77fd85f4602d3
tusharsarkar3 <59759357+tusharsarkar3@users.noreply.github.com>
2021-02-28 03:00:06 +0530

    Update semantic_similarity.py

--- semantic_similarity.py
+++ semantic_similarity.py
@@ -1,8 +1,20 @@
-#from semantic_text_similarity.models import WebBertSimilarity
-from semantic_text_similarity.models import ClinicalBertSimilarity
+from sentence_transformers import SentenceTransformer
 
-#web_model = WebBertSimilarity(device='cpu', batch_size=10) #defaults to GPU prediction
+model = SentenceTransformer('paraphrase-distilroberta-base-v1')
 
-clinical_model = ClinicalBertSimilarity(device='cpu', batch_size=10) #defaults to GPU prediction
+def semantic_similarity_roberta(sentences):
+    sentence_embeddings = model.encode(sentences)
+    li = []
+    for sentence, embedding in zip(sentences, sentence_embeddings):
+        print("Sentence:", sentence)
+        li.append(embedding)
+        print("Embedding:", embedding.shape)
 
-print(clinical_model.predict([("Nishit plays badminton","Badminton is played by Nishit")])/5)
+    dot = sum(a*b for a, b in zip(li[0], li[1]))
+    norm_a = sum(a*a for a in li[0]) ** 0.5
+    norm_b = sum(b*b for b in li[1]) ** 0.5
+
+    cos_sim = dot / (norm_a*norm_b)
+
+    print(cos_sim)
+
