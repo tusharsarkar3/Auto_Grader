fa1e665e1bb6a38c32ef741849b962bd17103e4f
tusharsarkar3 <59759357+tusharsarkar3@users.noreply.github.com>
2021-02-28 02:58:24 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,33 +1,33 @@
 const fs = require('fs');
 const request = require('request');
 
-const options = {
-  method: 'POST',
-  url: 'https://pen-to-print-handwriting-ocr.p.rapidapi.com/recognize/',
-  headers: {
-    'content-type': 'multipart/form-data; boundary=---011000010111000001101001',
-    'x-rapidapi-key': 'YOUR KEY HERE',
-    'x-rapidapi-host': 'pen-to-print-handwriting-ocr.p.rapidapi.com',
-    useQueryString: true
-  },
-  formData: {
-    srcImg: {
-      value: fs.createReadStream('page0.jpg'),
-      options: {filename: 'page0.jpg', contentType: 'application/octet-stream'}
-    },
-    Session: 'string'
-  }
-};
+    const options = {
+      method: 'POST',
+      url: 'https://pen-to-print-handwriting-ocr.p.rapidapi.com/recognize/',
+      headers: {
+        'content-type': 'multipart/form-data; boundary=---011000010111000001101001',
+        "x-rapidapi-key": "915f758cb5mshbb558a904c64455p14aa0ajsnec22ba2f1c80",
+        'x-rapidapi-host': 'pen-to-print-handwriting-ocr.p.rapidapi.com',
+        useQueryString: true
+      },
+      formData: {
+        srcImg: {
+          value: fs.createReadStream('page1.jpg'),
+          options: {filename: 'page1.jpg', contentType: 'application/octet-stream'}
+        },
+        Session: 'string'
+      }
+    };
 
-request(options, function (error, response, body) {
-	if (error) throw new Error(error);
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
 
-	console.log(body);
-	fs.writeFile('Output.txt', body, (err) => {
+        console.log(body);
+        fs.writeFile('output.txt', body, (err) => {
 
-    // In case of a error throw err.
-    if (err) throw err;
-})
+        // In case of a error throw err.
+        if (err) throw err;
+    })
 
-    return body
-});
+        return body
+    });
